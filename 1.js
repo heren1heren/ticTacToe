82b0e9a0c4477728c896617050cb9c4c486cca1b
heren <heren1heren@gmail.com>
2023-11-19 11:35:54 -0700

    after the video

---
+++ 1.js
@@ -0,0 +1,14 @@
+const cellElements = document.querySelectorAll('[data-cell]');
+
+
+cellElements.forEach(cell => {
+  cell.addEventListener('click',handleClick,
+   {once:true})
+})
+
+
+
+function handleClick() {
+  //
+  //placeMark
+}
